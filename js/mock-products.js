fa1adb00c631dda912c7609f4d2084c76252efb8
jjswatch <39426340+jjswatch@users.noreply.github.com>
2025-12-26 16:18:58 +0800

    Add files via upload

---
+++ js/mock-products.js
@@ -0,0 +1,9 @@
+const MOCK_PRODUCTS = [
+  {
+    barcode: "6933544300111",
+    productId: 12,
+    productName: "干貝風味",
+    brand: "烹大師",
+    imageUrl: "https://cdn-next.cybassets.com/media/W1siZiIsIjIzNDEzL3Byb2R1Y3RzLzM4NjM5NTAyLzE2Njc3OTAzNzJfNDFiYjNmNmFjNDAwNTY0M2NjYjAuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=c37c47fd1e78d685"
+  }
+];
