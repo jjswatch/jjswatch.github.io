45e7b2eeb2634e68000dae1eb2772323e47e9feb
jjswatch <39426340+jjswatch@users.noreply.github.com>
2025-12-29 14:43:34 +0800

    Add files via upload

--- js/mock-products.js
+++ js/mock-products.js
@@ -1,9 +1,16 @@
 const MOCK_PRODUCTS = [
-  {
-    barcode: "6933544300111",
-    productId: 12,
-    productName: "干貝風味",
-    brand: "烹大師",
-    imageUrl: "https://cdn-next.cybassets.com/media/W1siZiIsIjIzNDEzL3Byb2R1Y3RzLzM4NjM5NTAyLzE2Njc3OTAzNzJfNDFiYjNmNmFjNDAwNTY0M2NjYjAuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=c37c47fd1e78d685"
-  }
+	{
+		barcode: "6933544300111",
+		productId: 12,
+		productName: "干貝風味",
+		brand: "烹大師",
+		imageUrl: "https://cdn-next.cybassets.com/media/W1siZiIsIjIzNDEzL3Byb2R1Y3RzLzM4NjM5NTAyLzE2Njc3OTAzNzJfNDFiYjNmNmFjNDAwNTY0M2NjYjAuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=c37c47fd1e78d685"
+	},
+	{
+		barcode: "8801062475162",
+		productId: 13,
+		productName: "黑巧克力派",
+		brand: "LOTTE",
+		imageUrl: "https://www.lottetaiwan.com.tw/files/20250911165203482.png"
+	}
 ];
